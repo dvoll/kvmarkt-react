8f6d8aa48978e6411b5fd0f3980a8c2f0a1cc962
DESKTOP-GTE3T9A\Dario <dariovoll@gmail.com>
2018-11-16 03:09:22 +0100

    Firestore Redux SideEffect Implementation

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,15 @@ import * as ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+
+
 import { Provider } from "react-redux";
 import { BrowserRouter } from 'react-router-dom'
 import App from "./App";
 import configureStore from "./configureStore";
+import { firestore } from "./initializeFirebase";
+
+firestore();
 
 ReactDOM.render(
   <BrowserRouter>
@@ -19,27 +24,4 @@ ReactDOM.render(
 );
 registerServiceWorker();
 
-
-const db = firebase.firestore();
-
-db.settings({
-    timestampsInSnapshots: true
-});
-
-db.collection("Schemes")
-  .get()
-  .then((querySnapshot: any) => {
-    querySnapshot.forEach((doc: any) => {
-      console.log(`${doc.id}`, doc.data());
-  });
-});
-
-const catRef = db.collection('scheme_categories').doc('1');
-
-db.collection('Schemes')
-  .where('place', '==', catRef.id)  
-  .get()
-  .then( (r: any) => {
-    r.forEach((n: any) => console.log('row', n.data()))
-  })
-  .catch( (err: any) => console.error(err) );
+// fire.initializeApp(firebaseConfig);
--- src/store/schemes/epics.ts
+++ src/store/schemes/epics.ts
@@ -1,7 +1,8 @@
 import { AnyAction } from "redux";
 import { Epic, ofType } from "redux-observable";
-import { from, of } from "rxjs";
+import { Observable, of } from "rxjs";
 import { catchError, delay, map, mergeMap, takeUntil, tap } from "rxjs/operators";
+import { firestore } from "src/initializeFirebase";
 import { addError, addSuccess, fetchError, fetchSuccess } from "./actions";
 import { Scheme, SchemesActionTypes } from "./types";
 
@@ -9,23 +10,43 @@ export const schemesRequestEpic: Epic<AnyAction, AnyAction, void> = (
     action$,
     state
 ) => {
-    return action$.pipe(
-        map(action => {
+    return action$.pipe( map(action => {
             console.log(action);
             return action;
-        }),
-        ofType(SchemesActionTypes.FETCH_REQUEST),
-        delay(1000),
-        mergeMap(action => from(fetch("./schemes.mock.json")).pipe(
-            tap(res => console.log("Response:", res)),
-            mergeMap( (response: Response) => response.json()),
-            tap(res => console.log("JSON:", res)),
-            map( (response: {result: Scheme[]}) => response.result),
-            map( (result) => fetchSuccess(result)),
-            catchError(error => of(fetchError(error))),
-            takeUntil(action$.pipe(ofType(SchemesActionTypes.FETCH_CANCELED)))
-        ))
-    )
+        }), 
+        ofType(SchemesActionTypes.FETCH_REQUEST), 
+        takeUntil(action$.pipe(ofType(SchemesActionTypes.FETCH_CANCELED))),
+        delay(1000), tap(), mergeMap(
+            () => {
+                return Observable.create((observer: any) => {
+                    firestore()
+                        .collection("Schemes")
+                        .onSnapshot({
+                            next: (snapshot: any) => {
+                                observer.next(snapshot);
+                            },
+                            error: (err: any) => {
+                                observer.error(err);
+                            },
+                            complete: () => {
+                                observer.complete();
+                            }
+                        });
+                    return () => {
+                        console.log("remove");
+                    };
+                });
+            }
+        ), map((querySnapshot: any) => {
+            // firestore.QuerySnapshot
+            return querySnapshot.docs.map((r: any) => {
+                const { id, title, description, ageStart, ageEnd, place, category, author } = r.data();
+                const scheme: Scheme = { id, title, place, category, description, ageStart, ageEnd, author };
+                return scheme;
+            });
+        }), map(schemes =>
+            fetchSuccess(schemes)
+        ), catchError(error => of(fetchError(error))) )
 };
 
 
